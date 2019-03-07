940691f62aa13b9fff7e5dd45f57a4fd71c7d0e0
davidlenner <david910128@gmail.com>
2019-03-07 15:25:05 +0100

    data displayed in table

--- src/main/client/angular/src/app/space-x/space-x.component.ts
+++ src/main/client/angular/src/app/space-x/space-x.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-space-x',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpaceXComponent implements OnInit {
 
-  constructor() { }
+  spaceXData: Object;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getSpaceXData().subscribe(data => this.spaceXData = data )
   }
 
+  getSpaceXData(){
+    return this.http.get("http://localhost:60150/spacex")
+  }
 }

4ef0aab350c3de8039ca7769540163fbbedd50e9
davidlenner <david910128@gmail.com>
2019-03-20 14:14:13 +0100

    SpaceX Pojo created

---
+++ src/main/java/com/lenner/personalprojectwithapis/models/SpaceXData.java
@@ -0,0 +1,65 @@
+package com.lenner.personalprojectwithapis.models;
+
+import javax.persistence.*;
+
+
+@Entity
+public class SpaceXData {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String missionName;
+    private String launchDateLocal;
+    private String payloadType;
+    private String payloadMassKg;
+    private String siteNameLong;
+    private String details;
+    private String rocketName;
+
+    public SpaceXData() {
+    }
+
+    public SpaceXData(String missionName, String launchDateLocal, String payloadType,
+                      String payloadMassKg, String siteNameLong, String details, String rocketName) {
+        this.missionName = missionName;
+        this.launchDateLocal = launchDateLocal;
+        this.payloadType = payloadType;
+        this.payloadMassKg = payloadMassKg;
+        this.siteNameLong = siteNameLong;
+        this.details = details;
+        this.rocketName = rocketName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getMissionName() {
+        return missionName;
+    }
+
+    public String getLaunchDateLocal() {
+        return launchDateLocal;
+    }
+
+    public String getPayloadType() {
+        return payloadType;
+    }
+
+    public String getPayloadMassKg() {
+        return payloadMassKg;
+    }
+
+    public String getSiteNameLong() {
+        return siteNameLong;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public String getRocketName() {
+        return rocketName;
+    }
+}
